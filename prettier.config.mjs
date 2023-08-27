2186672f47bd987b954ce7d7baf22c25b3cf8509
uxelexx <i@kkornilov.ru>
2023-08-27 18:28:13 +0300

    feat(prettier): prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @type {import('prettier').Config & import("@ianvs/prettier-plugin-sort-imports").PluginConfig}
+ */
+const config = {
+  arrowParens: "avoid",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+};
+
+export default config;
