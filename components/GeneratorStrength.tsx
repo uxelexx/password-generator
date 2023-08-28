b970c3d68936a039ef1162d2974665db9f864df1
uxelexx <i@kkornilov.ru>
2023-08-28 21:20:40 +0300

    feat(generator strength): init

---
+++ components/GeneratorStrength.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { calculateStrength } from "../helpers/calculateStrength";
+
+type Props = {
+  password: string;
+};
+
+export default function GeneratorStrength({ password }: Props) {
+  const strength = calculateStrength(password);
+
+  return (
+    <div className="uppercase w-full flex justify-between items-center p-6 bg-slate-950/60">
+      <p className="text-lg text-neutral-300 text-opacity-70">Strength</p>
+      <span className={`text-${strength.color}-400 text-2xl font-semibold`}>
+        {strength.value}
+      </span>
+    </div>
+  );
+}
