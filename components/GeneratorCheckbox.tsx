348a438b467adc5370223123bff893bf90209ba0
uxelexx <i@kkornilov.ru>
2023-08-28 21:20:49 +0300

    feat(generator checkbox): init

---
+++ components/GeneratorCheckbox.tsx
@@ -0,0 +1,16 @@
+import React, { type InputHTMLAttributes } from "react";
+
+type Props = InputHTMLAttributes<HTMLInputElement> & {
+  children: React.ReactNode;
+};
+export default function GeneratorCheckbox({ children, ...props }: Props) {
+  console.log("checkbox render");
+  return (
+    <div className="flex space-x-5 items-center">
+      <input id={props.name} {...props} type="checkbox" />
+      <label htmlFor={props.name} className="capitalize">
+        {children}
+      </label>
+    </div>
+  );
+}
