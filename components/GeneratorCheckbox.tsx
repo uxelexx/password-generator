0b2079223b7adea77b55b8643c089c64547fe01e
uxelexx <i@kkornilov.ru>
2023-08-28 21:52:22 +0300

    style(generator checkbox): custom checkbox

--- components/GeneratorCheckbox.tsx
+++ components/GeneratorCheckbox.tsx
@@ -3,11 +3,31 @@ import React, { type InputHTMLAttributes } from "react";
 type Props = InputHTMLAttributes<HTMLInputElement> & {
   children: React.ReactNode;
 };
+
 export default function GeneratorCheckbox({ children, ...props }: Props) {
   return (
-    <div className="flex space-x-5 items-center">
-      <input id={props.name} {...props} type="checkbox" />
-      <label htmlFor={props.name} className="capitalize">
+    <div className="flex space-x-5 items-center relative">
+      <div className="grid place-items-center">
+        <input
+          id={props.name}
+          className="peer cursor-pointer shrink-0 checked:bg-primary h-5 w-5 bg-transparent appearance-none border-2 border-primary"
+          {...props}
+          type="checkbox"
+        />
+        <svg
+          className="absolute text-bg-start top-[6px] left-[2px] pointer-events-none w-4 h-4 hidden peer-checked:block"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <polyline points="20 6 9 17 4 12"></polyline>
+        </svg>
+      </div>
+      <label htmlFor={props.name} className="text-lg capitalize">
         {children}
       </label>
     </div>
