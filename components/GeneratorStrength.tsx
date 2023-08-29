ca4d3965ad9cda6b0eac132c116b5482e2416eed
uxelexx <i@kkornilov.ru>
2023-08-29 12:59:17 +0300

    style(generator strength): responsivnes

--- components/GeneratorStrength.tsx
+++ components/GeneratorStrength.tsx
@@ -10,8 +10,8 @@ export default function GeneratorStrength({ password }: Props) {
 
   return (
     <div className="uppercase w-full h-20 flex justify-between items-center p-6 bg-slate-950/60">
-      <p className="text-lg text-neutral-100 text-opacity-70">Strength</p>
-      <span className="text-2xl font-semibold flex items-center">
+      <p className="sm:text-lg text-neutral-100 text-opacity-70">Strength</p>
+      <span className="sm:text-2xl text-xl font-semibold flex items-center">
         {value} <StrengthColumn color={color} strength={value} />
       </span>
     </div>
