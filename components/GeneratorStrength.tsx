9642f2dc0bc3ab9e13c82d113e7076e1205dc664
uxelexx <i@kkornilov.ru>
2023-08-29 11:40:50 +0300

    feat(generator strength): add strength column

--- components/GeneratorStrength.tsx
+++ components/GeneratorStrength.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { calculateStrength } from "../helpers/calculateStrength";
+import { StrengthColumn } from "./StrengthColumn";
 
 type Props = {
   password: string;
 };
 
 export default function GeneratorStrength({ password }: Props) {
-  const strength = calculateStrength(password);
+  const { value, color } = calculateStrength(password);
 
   return (
     <div className="uppercase w-full flex justify-between items-center p-6 bg-slate-950/60">
       <p className="text-lg text-neutral-200 text-opacity-70">Strength</p>
-      <span className={`text-${strength.color}-400 text-2xl font-semibold`}>
-        {strength.value}
+      <span className="text-2xl font-semibold flex items-center">
+        {value} <StrengthColumn color={color} strength={value} />
       </span>
     </div>
   );
