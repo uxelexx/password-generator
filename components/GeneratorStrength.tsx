7e14786a5bf0ddad81d02255e670cab0c732a45d
uxelexx <i@kkornilov.ru>
2023-08-29 12:21:51 +0300

    style(generator strength): add height

--- components/GeneratorStrength.tsx
+++ components/GeneratorStrength.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { calculateStrength } from "../helpers/calculateStrength";
 import { StrengthColumn } from "./StrengthColumn";
 
@@ -10,7 +9,7 @@ export default function GeneratorStrength({ password }: Props) {
   const { value, color } = calculateStrength(password);
 
   return (
-    <div className="uppercase w-full flex justify-between items-center p-6 bg-slate-950/60">
+    <div className="uppercase w-full h-20 flex justify-between items-center p-6 bg-slate-950/60">
       <p className="text-lg text-neutral-200 text-opacity-70">Strength</p>
       <span className="text-2xl font-semibold flex items-center">
         {value} <StrengthColumn color={color} strength={value} />
