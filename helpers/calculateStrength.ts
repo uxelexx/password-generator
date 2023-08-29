1479b43275b853d67a7eec8720d86c963f9d1061
uxelexx <i@kkornilov.ru>
2023-08-29 12:21:00 +0300

    feat(calc strength): add initial value

--- helpers/calculateStrength.ts
+++ helpers/calculateStrength.ts
@@ -8,27 +8,34 @@ type Strength = {
 const defaultOptions = [
   {
     id: 0,
-    value: "Too weak",
-    color: "tomato",
+    value: " ",
+    color: "",
     minDiversity: 0,
     minLength: 0,
   },
   {
     id: 1,
+    value: "Too weak",
+    color: "tomato",
+    minDiversity: 1,
+    minLength: 1,
+  },
+  {
+    id: 2,
     value: "Weak",
     color: "darkorange",
     minDiversity: 2,
     minLength: 6,
   },
   {
-    id: 2,
+    id: 3,
     value: "Medium",
     color: "khaki",
     minDiversity: 3,
     minLength: 8,
   },
   {
-    id: 3,
+    id: 4,
     value: "Strong",
     color: "lightgreen",
     minDiversity: 4,
